c7a873d9f103159898ff367fee529e05ee1af7ee
Jasmine Cheng <t-jache@microsoft.com>
2019-07-21 23:20:50 -0700

    Initial commit with Web-Extension-Starter

---
+++ src/ts/background/index.ts
@@ -0,0 +1,17 @@
+import { createStore } from "redux";
+import reducers, { IAppState, loadState } from "./store";
+import { wrapStore, Store } from "react-chrome-redux";
+import { configureApp } from './AppConfig';
+//import { persistStore } from "redux-persist";
+
+const preloadedState = loadState();
+const store: Store<IAppState> = createStore(reducers, preloadedState);
+
+configureApp(store);
+
+wrapStore(store, {
+	portName: 'ExPort' // Communication port between the background component and views such as browser tabs.
+});
+
+//const persistor = persistStore(store);
+
---
+++ src/ts/background/store/index.ts
@@ -0,0 +1,49 @@
+import settings, { IAppSettings } from './reducers/settings';
+import { combineReducers } from "redux";
+import counter, { ICounter } from './reducers/counter';
+
+import "redux";
+// Enhance the Action interface with the option of a payload. 
+// While still importing the Action interface from redux.
+declare module "redux" {
+	export interface Action<T = any, P = any> {
+		type: T;
+		payload?: P;
+	}
+}
+
+export interface IAppState {
+	counter: ICounter;
+	settings: IAppSettings;
+}
+
+export const loadState = ():IAppState | undefined => {
+	try {
+		const serializedState = localStorage.getItem('appstate');
+		if (serializedState === null) {
+			return undefined;
+		}
+		return JSON.parse(serializedState);
+	} catch (err) {
+		return undefined;
+	}
+};
+
+export const saveState = (appstate: IAppState, 
+						  success: () => void = () => {}, 
+						  error: (e: Error) => void = () => {}) => {
+	try {
+		const serializedState = JSON.stringify(appstate);
+		localStorage.setItem('appstate', serializedState);
+		success()
+	} catch(e) {
+		error(e)
+	}
+};
+
+const reducers = combineReducers<IAppState>({
+	counter,
+	settings
+});
+
+export default reducers;
