9b8a8d8e38524cbf5584bebc059b0760675ccadc
monicak485 <monica_k@gwu.edu>
2019-07-23 12:04:13 -0700

    fixing errors in similarity

--- src/ts/contentScripts/extraction/index.tsx
+++ src/ts/contentScripts/extraction/index.tsx
@@ -2,10 +2,19 @@ import {Extractor} from "./Extractor"
 import { SimilarityChecker } from '../similarity/similarity';
 import {Listener} from "./Listener/AmazonListener"
 
+// Extractor
 var extractor = new Extractor(false);
 extractor.getPage();
 extractor.getProduct();
+
+// Similarity 
 var similarityChecker1 = new SimilarityChecker();
-similarityChecker1.getCurrentProdData();
+var similarityFound = similarityChecker1.isSimilar();
+if (similarityFound)
+    console.log("Similarity Found");
+else
+    console.log("No Similarity Found");
+
+// Listener
 var listener = new Listener(false);
 listener.TestButton();
