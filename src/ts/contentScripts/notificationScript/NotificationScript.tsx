5043af34f5345453df33fd8b02f8ccedc19f8920
monicak485 <monica_k@gwu.edu>
2019-07-24 10:31:41 -0700

    Dummy change

--- src/ts/contentScripts/notificationScript/NotificationScript.tsx
+++ src/ts/contentScripts/notificationScript/NotificationScript.tsx
@@ -73,7 +73,7 @@ class NotificationScript extends React.Component<INotificationScript> {
                 cost: extractorProduct.getPrice() as number,
                 description: description ? description : "",
             }
-            //alert('Found product ' + JSON.stringify(product));
+            //alert('Found product ' +  JSON.stringify(product));
             if(this.similarityChecker.isSimilar(product)) {
                 //alert('Similar product ' + JSON.stringify(product));
                 this.props.dispatch(newNotif({notificationType: 'SIMILAR', product: product}))
